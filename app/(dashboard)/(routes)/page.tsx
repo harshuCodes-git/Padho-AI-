512e529ccbb0170a58a32d2148272ed066bb92f5
harshuCodes-git <1032210414@tcetmumbai.in>
2024-06-18 20:32:10 +0530

    Crossed Update

---
+++ app/(dashboard)/(routes)/page.tsx
@@ -0,0 +1,14 @@
+import {UserButton} from "@clerk/nextjs"
+
+export default function Home() {
+  return (
+    <div>
+      <div>
+        <UserButton
+        afterSignOutUrl="/"
+        />
+      </div>
+      
+    </div>
+  );
+}
